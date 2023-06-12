8387f8742c80f95d524097da604de2927249d078
Maximilian Broszio <brosziomax@posteo.de>
2023-06-12 22:47:35 +0200

    Follower relation creation

--- backend/db.py
+++ backend/db.py
@@ -16,16 +16,28 @@ def setup_db(app: Flask):
 
         sys_db.create_database('twitter2')
         db = client.db(name='twitter2', username='root', password='passwd')
-        print(db.collections())
-        if not db.has_collection('users'):
-            users = db.create_collection('users', user_keys=True)
         g.db = db
         initial_data_load(db)
 
 
 def initial_data_load(db: StandardDatabase):
+    create_users(db)
+    create_relations(db)
+
+
+def create_users(db: StandardDatabase):
+    if not db.has_collection('users'):
+        users_collection = db.create_collection('users')
+    else:
+        users_collection = db.collection('users')
     users = file_reader.read_users()
-    users_collection = db.collection('users')
-    success = users_collection.insert_many(users, overwrite=True)
+    users_collection.insert_many(users, overwrite=True)
     print("Created users")
 
+
+def create_relations(db: StandardDatabase):
+    if not db.has_collection('follows'):
+        follows_edge_collection = db.create_collection('follows', edge=True)
+    else:
+        follows_edge_collection = db.collection('follows')
+    file_reader.read_relations(follows_edge_collection)
--- backend/file_reader.py
+++ backend/file_reader.py
@@ -1,5 +1,6 @@
 import os
 from mimesis import Person
+from arango.collection import StandardCollection
 import time
 
 
@@ -21,5 +22,32 @@ def read_users():
         for u in unique_user_ids:
             users.append({"_key": u, "name": person.full_name()})
         end = time.time()
-        print("Finished creating users, took {} seconds".format(end - start))
+        print("Finished creating {} users, took {} seconds".format(len(users), end - start))
         return users
+
+
+def read_relations(collection: StandardCollection):
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_path, 'resources/twitter_combined.txt')
+    with open(file_path, 'r', encoding='utf8') as f:
+        follower_relations = []
+        start = time.time()
+        count = 0
+        for line in f:
+            line = line.rstrip("\n")
+            user_ids = line.split(" ")
+            count += 1
+            if len(user_ids) != 2:
+                continue
+            if not user_ids[0] or not user_ids[1]:
+                continue
+            follower_relations.append({"_from": "users/" + user_ids[0], "_to": "users/" + user_ids[1]})
+            if len(follower_relations) > 10000:
+                collection.insert_many(follower_relations, overwrite=True)
+                follower_relations = []
+            if count % 250000 == 0:
+                print("Created {} follower relations".format(count))
+
+        collection.insert_many(follower_relations, overwrite=True)
+        end = time.time()
+        print("Finished creating {} follower relations, took {} seconds".format(count, end - start))
